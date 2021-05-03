2d24ca6717eae7afb1b5de8d70055f3eb806998c
Pablo Raphael <pabloraphaelmoreira@outlook.com>
2021-05-03 17:12:34 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsvendaS/DsvendaSApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsvendaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsvendaSApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsvendaS/DsvendaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsvendaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
