ebdda858a0f9bbee10c5f1ae1edb30201b1a9091
Pablo Raphael <pabloraphaelmoreira@outlook.com>
2021-05-05 13:15:21 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendaS/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsvendaS.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dsvendaS.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
